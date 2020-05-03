64c348b8e7ace59a6f9a9eb93b75f8ae8f1623ff
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2020-05-03 20:14:33 +0300

    Add public lists component.

---
+++ src/app/public-lists/public-lists.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-public-lists',
+  templateUrl: './public-lists.component.html',
+  styleUrls: ['./public-lists.component.css']
+})
+export class PublicListsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
