c0a57dea0e996137d08230e44148956bdaf79055
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2020-05-04 23:25:40 +0300

    Add test list getting to OnInit.
    This will get moved to somewhere else later.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Skribblist';
+  wordlists = [];
+
+  ngOnInit(): void {
+    this.updateList();
+  }
+
+  updateList(): void {
+    this.wordlists = [];
+    fetch('http://localhost:8080/lists')
+    .then((resp) => resp.json())
+    .then((lists) => {
+      console.log(lists);
+      lists.forEach((element) => {
+        this.wordlists.push(element);
+      });
+    });
+  }
+
 }
