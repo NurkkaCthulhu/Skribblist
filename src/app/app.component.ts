153797e6221391736d8e56796178e153880957ab
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2020-05-16 17:22:12 +0300

    Angular material add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,11 @@ export class AppComponent implements OnInit {
   title = 'Skribblist';
   wordlists = [];
   baseUrl = environment.baseUrl;
+  tooltipContent: string;
 
   ngOnInit(): void {
     this.updateList();
+    this.tooltipContent = 'Click to copy the words';
   }
 
   updateList(): void {
@@ -32,6 +34,26 @@ export class AppComponent implements OnInit {
       });
       console.log(this.wordlists);
     });
+  }
+
+  copyWords(val: string){
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = val;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+    this.tooltipContent = 'Copied!';
+
+    setTimeout( () => {
+      this.tooltipContent = 'Click to copy the words';
+    }, 1500);
+
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PublicListsComponent } from './public-lists/public-lists.component';
 import { NotfoundComponent } from './notfound/notfound.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { NotfoundComponent } from './notfound/notfound.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTooltipModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
