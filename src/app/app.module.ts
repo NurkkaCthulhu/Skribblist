6662de66afd7ab52fa4d45d05ea397c2e45d492f
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2020-05-18 20:50:39 +0300

    Create create list code.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {MatTooltipModule} from '@angular/material/tooltip';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PublicListsComponent } from './public-lists/public-lists.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatExpansionModule} from '@angular/material/expansion';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { CreateListComponent } from './create-list/create-list.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -22,7 +24,9 @@ import { CreateListComponent } from './create-list/create-list.component';
     AppRoutingModule,
     MatTooltipModule,
     BrowserAnimationsModule,
-    MatExpansionModule
+    MatExpansionModule,
+    FormsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/create-list/create-list.component.ts
+++ src/app/create-list/create-list.component.ts
@@ -10,31 +10,61 @@ import SkribblList from '../SkribblList';
 export class CreateListComponent implements OnInit {
 
   baseUrl = environment.baseUrl;
-  userList: SkribblList = {words: 'sana,toinen,kolmas,terve,heihei',
-                          code: 'thisMineCode',
-                          info: 'This is a list. Enjoy!',
-                          list_name: 'This is the posted list.',
+  userInput: SkribblList = {words: '',
+                          code: 'coolCodeHere',
+                          info: '',
+                          list_name: '',
                           public: true
                         };
+  newWord: string = '';
+  allWords = [];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  disableSubmit() {
+    return !(this.allWords.length >= 4 && this.userInput.list_name.length > 0);
+  }
+
+  addWordToList() {
+    if (this.allWords.includes(this.newWord)) {
+      alert("Can't add a word that's already on the list.");
+    } else {
+      this.allWords.push(this.newWord.toLowerCase());
+      this.newWord = '';
+    }
+  }
+
+  removeFromWordList(word: string) {
+    this.allWords = this.allWords.filter(e => e !== word);
+  }
+
   createList() {
+    const allWordsAsString = this.allWords.join(',');
+    this.userInput.words = allWordsAsString;
     fetch(this.baseUrl + 'lists', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(this.userList),
+      body: JSON.stringify(this.userInput),
     })
     .then(response => {
       return response.json();
     })
     .then(data => {
       console.log('Success; posted a list called:', data.name);
+      this.userInput = {words: '',
+                          code: 'coolCodeHere',
+                          info: '',
+                          list_name: '',
+                          public: true
+                        };
+      this.newWord = '';
+      this.allWords = [];
+      alert('List posted successfully!');
     })
     .catch((error) => {
       console.error('Error:', error);
@@ -42,7 +72,7 @@ export class CreateListComponent implements OnInit {
   }
 
   deleteList() {
-    fetch(this.baseUrl + 'lists/2', {
+    fetch(this.baseUrl + 'lists/16', {
       method: 'DELETE',
     })
     .then(res => res.json())
