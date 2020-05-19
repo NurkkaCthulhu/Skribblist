a62c618f5d5e11a59c7fada958e5f09cce2a1247
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2020-05-19 20:09:14 +0300

    Update server.js

--- server.js
+++ server.js
@@ -22,10 +22,10 @@ app.listen(process.env.PORT || 8080, () => {
   console.log('App running');
 });
 
-/*
+
 app.get('/*', (req, res) =>
     res.sendFile(path.join(__dirname + '/dist/Skribblist/index.html')),
-);*/
+);
 /*
 app.get('/', (req, res) => {
     res.json({ info: 'Node.js, Express, and Postgres API' })
